6b267c9239d7d4cae4d037c7d7a0d19ffcd9586c
naveen <neonew8984@gmail.com>
2021-07-16 22:00:19 +0530

    Route For Homepage

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
+
 
 const app = express();
 
 const PORT = 5000;
 
-app.use(bodyParser.json);
+
+
+app.get("/", (req, res) => {
+  console.log("Testing");
+  res.send("Hello From Homepage");
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running on Port: ${PORT}`);
 });
- 
